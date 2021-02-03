d2302e128662c4d3dc6fd14f5c457f224acbd100
seilylook@naver.com <seilylook@naver.com>
2021-02-03 16:20:04 +0900

    1 commit

---
+++ src/solution.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class solution {
+
+    public static int[][] matrix;
+    public static Queue<Integer> qu = new LinkedList<>();
+    public static int[] flag;
+    public static int N,M;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        matrix = new int[N+1][N+1];
+
+        for (int i=0; i<M; i++) {
+            int vertex1 = sc.nextInt();
+            int vertex2 = sc.nextInt();
+            matrix[vertex1][vertex2] = 1;
+        }
+
+        flag = new int[N+1];
+
+        bfs(1);
+    }
+
+    public static void bfs(int start) {
+        qu.offer(start);
+        flag[start] = 1;
+        int result = 0;
+
+        while(!qu.isEmpty()) {
+            start = qu.poll();
+
+            for (int i = 0; i < flag.length; i++) {
+                if(flag[i]!=1 && matrix[start][i] ==1){
+                    qu.offer(i);
+                    flag[i] = 1;
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
