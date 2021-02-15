a94c5eb7869f716be1421cd9ea0f8d25632004a3
seilylook@naver.com <seilylook@naver.com>
2021-02-15 19:35:38 +0900

    1 commit

---
+++ src/solution.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class solution {
+    static int N,M;
+    static int[][] matrix;
+    static boolean[] flag;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        matrix = new int[N][M];
+
+        for (int i = 0; i < M; i++) {
+            int vertex1 = Integer.parseInt(br.readLine());
+            int vertex2 = Integer.parseInt(br.readLine());
+
+            matrix[vertex1][vertex2] = 1;
+        }
+
+        flag = new boolean[N];
+        int result = 0;
+
+        for (int i = 0; i <= N; i++) {
+            if(!flag[i]){
+                dfs(i);
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static void dfs(int start){
+        if(flag[start] == true){
+            return;
+        }
+        else{
+            for (int i=0; i<=N; i++){
+                if(matrix[start][i] == 1){
+                    dfs(i);
+                }
+            }
+        }
+    }
+}
